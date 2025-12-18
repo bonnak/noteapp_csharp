ddc68949076e588ca07ae9cbc3a4d7625daacdd8
Bonnak <chea.bonnak@gmail.com>
2025-12-18 14:09:54 +0700

    User model

---
+++ WebApi/Models/User.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace WebApi.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Username { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
