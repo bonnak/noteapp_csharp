37f2d957f6ed3bf669c47afcb705555621180f3a
Bonnak <chea.bonnak@gmail.com>
2025-12-19 00:21:13 +0700

    Cors allow all

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -43,6 +43,16 @@ builder.Services.AddAuthorization();
 
 builder.Services.AddScoped<JwtService>();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -56,6 +66,8 @@ if (app.Environment.IsDevelopment())
 app.MapControllers();
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
 
 app.UseAuthentication();
 
