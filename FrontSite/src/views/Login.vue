f033dd9aeab8ff644638a9d0e33e592c0f8dff1e
Bonnak <chea.bonnak@gmail.com>
2025-12-19 00:06:40 +0700

    Install tailwindcss

---
+++ FrontSite/src/views/Login.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="flex min-h-full flex-col py-12 sm:px-6 lg:px-8">
+    <div class="sm:mx-auto sm:w-full sm:max-w-md">
+      <img class="mx-auto h-10 w-auto" src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=teal&shade=600" alt="Your Company" />
+      <h2 class="mt-6 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Note App</h2>
+    </div>
+
+    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
+      <div class="bg-white px-4 py-8 shadow-sm sm:rounded-lg sm:px-12">
+        <form class="space-y-6" action="/" method="POST">
+          <div>
+            <label for="username" class="block text-sm/6 font-medium text-gray-900">Username</label>
+            <div class="mt-2">
+              <input type="username" name="username" id="username" value="" autocomplete="username" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-teal-600 sm:text-sm/6" />
+            </div>
+          </div>
+
+          <div>
+            <label for="password" class="block text-sm/6 font-medium text-gray-900">Password</label>
+            <div class="mt-2">
+              <input type="password" name="password" id="password" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-teal-600 sm:text-sm/6" />
+            </div>
+          </div>
+
+          <div>
+            <button type="submit" class="cursor-pointer flex w-full justify-center rounded-md bg-teal-600 px-3 py-2 text-sm/6 font-semibold text-white shadow-xs hover:bg-teal-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-600">Sign in</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+
+</template>
