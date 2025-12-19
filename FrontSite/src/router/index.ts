274de5ef0e0a7340a05c9e2c01e35ebebe6e0cfa
Bonnak <chea.bonnak@gmail.com>
2025-12-19 08:36:31 +0700

    Redirect if authenticated

--- FrontSite/src/router/index.ts
+++ FrontSite/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import { useAuthStore } from '@/stores/auth'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,18 +9,34 @@ const router = createRouter({
       path: '/register',
       name: 'register',
       component: () => import('../views/Register.vue'),
+      meta: { redirectIfAuthenticated: true },
     },
     {
       path: '/login',
       name: 'login',
       component: () => import('../views/Login.vue'),
+      meta: { redirectIfAuthenticated: true },
     },
     {
       path: '/',
       name: 'home',
       component: HomeView,
+      meta: {
+        requireAuth: true,
+      },
     },
   ],
+})
+
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore()
+  if (to.meta.requireAuth && !authStore.isAuthenticated) {
+    next('/login')
+  } else if (to.meta.redirectIfAuthenticated && authStore.isAuthenticated) {
+    next('/')
+  } else {
+    next()
+  }
 })
 
 export default router
