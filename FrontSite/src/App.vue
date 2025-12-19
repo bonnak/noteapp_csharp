9b9916ac04870aee9c02590a736d7747d77b0d82
Bonnak <chea.bonnak@gmail.com>
2025-12-19 13:12:37 +0700

    Remove padding

--- FrontSite/src/App.vue
+++ FrontSite/src/App.vue
@@ -3,7 +3,7 @@ import HeaderNav from './components/HeaderNav.vue'
 </script>
 
 <template>
-  <div class="h-full flex flex-col flex-grow lg:pl-72">
+  <div class="h-full flex flex-col flex-grow">
     <HeaderNav />
 
     <RouterView />
